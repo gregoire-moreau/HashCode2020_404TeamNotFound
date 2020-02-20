4395fa42cc7ef2c5ae0994f99cfb48afd76a157d
Guillaume Everarts de Velp <guillaume.everarts@student.uclouvain.be>
2020-02-20 21:23:53 +0100

    merge

--- qualification_round/evaluate.py
+++ qualification_round/evaluate.py
@@ -6,9 +6,26 @@ def evaluate_count(library, remaining_days, book_scores):
     return score, library[4][:score]
 
 
-def evaluate(library, remaining_days, book_scores):
+def evaluate_value(library, remaining_days, book_scores):
+    number, books = evaluate_count(library, remaining_days, book_scores)
+    score = 0
+    for book in books:
+        score += book_scores[book]
+    return score, books
+
+
+def evaluate_ratio(library, remaining_days, book_scores):
     number, books = evaluate_count(library, remaining_days, book_scores)
     score = 0
     for book in books:
         score += book_scores[book]
     return score / library[2], books
+
+
+def evaluate_setup(library, remaining_days, book_scores):
+    number, books = evaluate_count(library, remaining_days, book_scores)
+    return remaining_days - library[2], books
+
+
+def evaluate(library, remaining_days, book_scores):
+    pass
