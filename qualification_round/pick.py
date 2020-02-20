02285599a226803deba4651bb4923b55eeba707d
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2020-02-20 20:27:34 +0100

    First submission

--- qualification_round/pick.py
+++ qualification_round/pick.py
@@ -3,5 +3,6 @@ def choose_library(best_library, output, libraries, library_books, best_books):
     output.append((best_library, best_books))
     for book in best_books:
         for lib in library_books[book]:
-            libraries[lib][4].remove(book)
+            if lib in libraries:
+                libraries[lib][4].remove(book)
     del libraries[best_library]
