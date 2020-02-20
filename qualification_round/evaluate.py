774b72b12fa512acc7d910ae7deaf5a746f0a39a
Guillaume Everarts de Velp <guillaume.everarts@student.uclouvain.be>
2020-02-20 20:59:46 +0100

    new heuristic

--- qualification_round/evaluate.py
+++ qualification_round/evaluate.py
@@ -1,4 +1,12 @@
 
-def evaluate(library, remaining_days, book_scores):
+def evaluate_count(library, remaining_days, book_scores):
     score = min((remaining_days - library[2]) * library[3], len(library[4]))
     return score, library[4][:score]
+
+
+def evaluate(library, remaining_days, book_scores):
+    number, books = evaluate_count(library, remaining_days, book_scores)
+    score = 0
+    for book in books:
+        score += book_scores[book]
+    return score, books
